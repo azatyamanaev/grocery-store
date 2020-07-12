1a9bd988930c878518c75c65ff91495f7054376d
RuslanKorchenov <ruselkomp1@gmail.com>
2020-07-12 15:46:16 +0300

    First security changes

---
+++ src/main/java/ru/itis/grocerystore/security/details/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package ru.itis.grocerystore.security.details;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.itis.grocerystore.models.State;
+import ru.itis.grocerystore.models.User;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDetailsImpl<T extends User> implements UserDetails {
+
+    private T user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole().toString());
+        return Collections.singleton(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getState() == State.CONFIRMED;
+    }
+}
